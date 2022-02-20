198848de15e069e14ee5c0a46b6113c82d80132e
Egle Grublyte <egle.grublyte@gmail.com>
2022-02-20 09:45:15 +0100

    fix main imports bug and update installables

--- taming/util.py
+++ taming/util.py
@@ -1,6 +1,9 @@
-import os, hashlib
+import importlib
+import os
+import hashlib
 import requests
 from tqdm import tqdm
+
 
 URL_MAP = {
     "vgg_lpips": "https://heibox.uni-heidelberg.de/f/607503859c864bc1b30b/?dl=1"
@@ -140,6 +143,20 @@ def retrieve(
         return list_or_dict
     else:
         return list_or_dict, success
+
+
+def instantiate_from_config(config):
+    if not "target" in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
+
+
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
 
 
 if __name__ == "__main__":
